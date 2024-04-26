b533629e800d5ce7eb4a4cf39aa3c56d061bdb2b
Rushabh <rushabhkhandhar38@.com>
2024-04-26 16:40:53 +0530

    updated the properties file and my function

--- src/main/java/com/example/demo/Bankapplication.java
+++ src/main/java/com/example/demo/Bankapplication.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
 public class Bankapplication {
-    
+    public static void main(String[] args) {
+        SpringApplication.run(Bankapplication.class, args);
+    }
 }
